e04a77d0b87b4936294b2d99255409c36ee8955f
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-06-26 11:17:34 +0700

    initial commit

---
+++ SharpEnum/FsEnum/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace FsEnum
+
+open SharpEnum
+
+type Class1() = 
+    member this.X = "F#"
+
+
+module Test =
+    let a = SharpEnum.Character.A
+
+    let value = 
+        match a with
+        | Character.A -> "A"
+        | Character.B -> "B"
+        | Character.C -> "C"
+        | _ -> "D"
+
---
+++ SharpEnum/SharpEnum/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEnum
+{
+    public enum Letter { A, B }
+
+    class Test
+    {
+        public String CheckEnum(bool input)
+        {
+            switch (input)
+            {
+                case true: return "Yes";
+                case false:  return "No";
+            }
+        }
+        public String GetValue1(Letter a)
+        {
+            if (a == Letter.A) return "a";
+            else if (a == Letter.B) return "b";
+            return "";
+        }
+
+        public String GetValue2(Letter a)
+        {
+            switch (a)
+            {
+                case Letter.A:
+                    return "a";
+                case Letter.B:
+                    return "b";
+                default:
+                    return "z";
+            }
+        }
+
+        public void ShouldCover()
+        {
+            var a = Letter.A;
+            var b = Letter.B;
+            var d = (Letter)100;
+            var e = (Letter)200;
+            var f = (Letter)300;
+        }
+    }
+}
